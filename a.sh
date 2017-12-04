9e9ee60803279dd9adb973a9f3f672d577dbb924
dongmeichang <33536535+dongmeichang@users.noreply.github.com>
2017-12-04 18:49:39 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,29 +1,12 @@
 #!/bin/sh
 echo "hello world"
 mkdir /hello
+mkdir /ttt
+touch /ttt/test.txt
 touch /hello/hello.txt
 mkdir -p /test/test1
 touch /test/test1/world.txt
-echo "hello hello" > /hello/hello.txt
-echo "world world" > /test/test1/world.txt
-echo "hello hello" > /hello/hello.txt
-echo "world world" > /test/test1/world.txt
-echo "hello hello" > /hello/hello.txt
-echo "world world" > /test/test1/world.txt
-echo "hello hello" > /hello/hello.txt
-echo "world world" > /test/test1/world.txt
-echo "hello hello" > /hello/hello.txt
-echo "world world" > /test/test1/world.txt
-echo "hello hello" > /hello/hello.txt
-echo "world world" > /test/test1/world.txt
-echo "hello hello" > /hello/hello.txt
-echo "world world" > /test/test1/world.txt
-echo "hello hello" > /hello/hello.txt
-echo "world world" > /test/test1/world.txt
-echo "hello hello" > /hello/hello.txt
-echo "world world" > /test/test1/world.txt
-echo "hello hello" > /hello/hello.txt
-echo "world world" > /test/test1/world.txt
+echo "test test" > test.txt
 echo "hello hello" > /hello/hello.txt
 echo "world world" > /test/test1/world.txt
 cp -r /hello/* $ALAUDACI_UPLOAD_DIR

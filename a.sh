7b014712ac83731ee005882804d0d6f0e963a366
dongmeichang <33536535+dongmeichang@users.noreply.github.com>
2017-12-06 10:02:13 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -7,4 +7,5 @@ mkdir -p /test/test1
 touch /test/test1/world.txt
 echo "hello hello" > /hello/hello.txt
 echo "world yuouououoiu world" > /test/test1/world.txt
+echo "welcome to here" > /test/jjjj.txt
 cp -r /hello/* $ALAUDACI_UPLOAD_DIR
